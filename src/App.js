f3d15b953a68dc7d3eca46c678c86a0fbe4adbf3
eprikhodko <eprikhodkodev@gmail.com>
2021-04-21 14:07:21 +0300

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css'
+import React from "react"
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import {Switch, Route} from "react-router-dom"
+
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path="/">
+                  <Photos />
+                </Route>
+                
+                <Route path ="/cart">
+                  <Cart />
+                </Route>
+            </Switch>
+        </div>
+    )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import "./styles.css"
+import {BrowserRouter as Router} from "react-router-dom"
+
 import App from './App'
 
+
+
 ReactDOM.render(
-    <App />,
+  <Router>
+      <App />
+  </Router>,
   document.getElementById('root')
 )
 
