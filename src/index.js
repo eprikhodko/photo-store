65360a41e089b5ef2a73f32ac368b2d028291be9
eprikhodko <eprikhodkodev@gmail.com>
2021-04-24 15:29:09 +0300

    Add Context to the App

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 import "./styles.css"
 import {BrowserRouter as Router} from "react-router-dom"
 
-import App from './App'
-
-
+import {ContextProvider} from "./Context"
+import App from "./App"
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
-  document.getElementById('root')
+    <ContextProvider>
+        <Router>
+            <App />
+        </Router>
+    </ContextProvider>, 
+    document.getElementById("root")
 )
-
