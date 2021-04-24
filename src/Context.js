8616927730ffab056efe215a63fb398b681b593b
eprikhodko <eprikhodkodev@gmail.com>
2021-04-24 16:19:42 +0300

    Add images.json

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,18 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
-        
+
+    useEffect(() => {
+        fetch("")
+            .then(response => response.json())
+            .then(data => setAllPhotos(data))
+    }, []) 
+    
+    console.log(allPhotos)
+
     return (
         <Context.Provider value={{allPhotos}}>
             {children}
